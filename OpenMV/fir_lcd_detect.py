4ec53ed18dd16b43ea1d64d8ad01a12099beb858
Chris Luke <chrisy@flirble.org>
2017-10-28 16:31:18 -0400

    Mostly cosmetic tweaks on the camera
    - Detect the largest blob and color its rectangle red.
    - Other blobs are now blue; fir frame is gray.
    - debug flag controls drawing anything into the frame.
    - Listen to the UART so the Pi can tell us to reset.
    - Tweak lux heuristic. Still not quite right but needs a field test.
    - Turn on the IR LEDs; doubt it will make much impact.
    - Increase image resolution; auto-calc fir region within it.
    - Adjust initial fps delay; the loop does more work now.

--- OpenMV/fir_lcd_detect.py
+++ OpenMV/fir_lcd_detect.py
@@ -2,21 +2,32 @@
 # transmitted over the UART pins (UART 3)
 #
 
-debug = True
-
 import sensor, image, time, fir, pyb
 
-print(repr(pyb.freq()))
+# If connected by USB, enable diagnostics
+# TODO: this detection doesn't work!
+#usb = pyb.USB_VCP()
+#debug = usb.isconnected()
+debug = True
+
+# Display the system clock rate
+if debug:
+    print(repr(pyb.freq()))
 
 # Setup the UART
 uart_bus = 1
 uart_baud = 115200
 uart = pyb.UART(uart_bus, baudrate=uart_baud, flow=0, bits=8, parity=None, stop=1, timeout_char=10)
 
+# LEDs
+led_ir = pyb.LED(4)
+led_ir.on()
+
 def send(text):
     global uart
-    print(text)
     uart.write("%s\r\n" % text)
+    if debug:
+        print(text)
 
 # Send an initial message
 send("")
@@ -30,7 +41,7 @@ sensor.set_contrast(1)
 sensor.set_brightness(0)
 sensor.set_saturation(2)
 sensor.set_pixformat(sensor.RGB565)
-sensor.set_framesize(sensor.QQVGA2)
+sensor.set_framesize(sensor.QVGA)
 
 # The following registers fine-tune the image
 # sensor window to align it with the FIR sensor.
@@ -46,19 +57,17 @@ fir.init()
 clock = time.clock()
 
 # fir region of display
-fir_height = 34
+fir_height = int(fir.height() * sensor.width() // fir.width())
 fir_yoffset = (sensor.height() // 2) - (fir_height // 2)
 fir_region = [0, fir_yoffset, sensor.width(), fir_height]
 fir_scale = [0, 35]
 
 # Our fir threshold range
-fir_threshold = [56, 73, 8, 69, -3, 76] # Middle L, A, B values.
-fir_threshold = [32, 95, -18, 40, -22, 92]
-# TODO need to tune this
+fir_threshold = [32, 95, -18, 40, -22, 92] # Middle L, A, B values.
 
 fps_target = 10
 fps_delay_max = 500
-fps_delay = 100
+fps_delay = 50
 fps_delay_inc = 1
 
 dbg_row = 0
@@ -107,8 +116,8 @@ if learn:
 def dbg(img, txt):
     global dbg_row
 
-    img.draw_string(1, dbg_row+1, txt, color = (0x00, 0x00, 0x00))
-    img.draw_string(0, dbg_row, txt, color = (0xff, 0xff, 0xff))
+    img.draw_string(1, dbg_row+1, txt, color=(0x00, 0x00, 0x00))
+    img.draw_string(0, dbg_row, txt, color=(0xff, 0xff, 0xff))
 
     dbg_row += 8
 
@@ -126,9 +135,11 @@ while(True):
     img = sensor.snapshot()
 
     # Calc luminosity (for some definition) of image and send to the Pi.
-    # Intention is to link iris diameter to brightness.
+    # Intention is to link iris dilation to brightness.
     stats = img.get_statistics()
-    lux = stats.uq() + stats.median()
+    lux = stats.uq()
+    if stats.mean() < 40:
+        lux += stats.median()
     if lux > 100:
         lux = 100
     send("lux:%d" % lux)
@@ -145,24 +156,40 @@ while(True):
     fir.draw_ir(img, ir, alpha=256, scale=fir_scale)
 
     # draw bounds of fir image
-    img.draw_rectangle(fir_region)
+    if debug:
+        img.draw_rectangle(fir_region, color=(0x33, 0x33, 0x33))
 
     # Do some detection on the FIR region of the image
     blob_count = 0
+    blob_largest = None
     for blob in img.find_blobs([fir_threshold],
             pixels_threshold=4, area_threshold=4,
             merge=True, margin=16,
             roi=fir_region):
         # display where the blob is
-        img.draw_rectangle(blob.rect())
+        if debug:
+            img.draw_rectangle(blob.rect(), color=(0x00, 0x00, 0xff))
 
         # tell rpi where the blob is
         send("blob:x=%1.2f:y=%1.2f:s=%1.1f" % (
-            blob.cx() / sensor.width(),
+           blob.cx() / sensor.width(),
             (blob.cy() - fir_yoffset) / fir_height,
             blob.area()/50
         ))
+
+        if debug:
+            b = (blob.rect(), blob.area())
+            if blob_largest:
+                if b[1] > blob_largest[1]:
+                    blob_largest = b
+            else:
+                blob_largest = b
+
         blob_count += 1
+
+    if debug and blob_largest:
+        img.draw_rectangle(blob_largest[0], color=(0xff, 0x00, 0x00))
+        blob_largest = None
 
     # Print FPS.
     fps = clock.fps()
@@ -182,3 +209,15 @@ while(True):
         dbg(img, "fps: %1.1f" % fps)
         dbg(img, "lux: %1.1f" % lux)
         dbg(img, "blobs: %d" % blob_count)
+        dbg(img, "delay: %d" % fps_delay)
+
+    # see if the Pi is trying to tell us something
+    while uart.any():
+        # read byte
+        b = uart.read(1)
+        if b == b"R":
+            # pi wants us to reset
+            send("reset:")
+            pyb.hard_reset()
+        else:
+            send("unknown:%s" % str(b))
